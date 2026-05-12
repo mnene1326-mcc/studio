945e4f43e7e5fb1879412db6b2d72ee91d74c1e0
Munene <mnene1326@gmail.com>
2026-05-12 09:38:43 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve 'li

--- src/components/layout/BottomNav.tsx
+++ src/components/layout/BottomNav.tsx
@@ -1,7 +1,7 @@
 
 "use client"
 
-import Link from "link"
+import Link from "next/link"
 import { usePathname } from "next/navigation"
 import { Home, MessageCircle, User } from "lucide-react"
 import { cn } from "@/lib/utils"
